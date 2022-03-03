f049c8b8faadddb47bf6d1b6f8fc5278fd0c68db
Kateryna <98611372+katushk-a@users.noreply.github.com>
2022-03-03 19:46:16 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+import { BankAccount, DebitAccount, CreditAccount } from './bankaccount.js';
+
 export class Client {
     surname;
     name;
@@ -11,5 +13,22 @@ export class Client {
         this.isActive = true;
         this.registerDate = new Date();
         this.bankAccounts = bankAccounts;
+    }
+
+    create() {
+        let clientDiv = document.createElement('div');
+        let nameSurname = document.createElement('p');
+        nameSurname.innerHTML = this.name + ' ' + this.surname;
+        clientDiv.append(nameSurname);
+        let active = document.createElement('p');
+        this.isActive ? active.innerHTML = 'ACTIVE' : active.innerHTML = 'NOT ACTIVE';
+        clientDiv.append(active);
+        let regdate = document.createElement('p');
+        regdate.innerHTML = this.registerDate;
+        clientDiv.append(regdate);
+        for (let bankAccount of this.bankAccounts) {
+            clientDiv.append(bankAccount.create());
+        }
+        return clientDiv;
     }
 }
